eac127385dd5aa9073de43b999e11e06c6525912
Orlando R Hernandez <47466479+acco-ohernandez@users.noreply.github.com>
2023-11-28 03:05:29 -0800

    Added the form

--- ScopeBoxes/Command1.cs
+++ ScopeBoxes/Command1.cs
@@ -12,6 +12,8 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 
+using ScopeBoxes.Forms;
+
 #endregion
 
 namespace ScopeBoxes
@@ -25,79 +27,113 @@ namespace ScopeBoxes
             UIApplication uiapp = commandData.Application;
             Document doc = uiapp.ActiveUIDocument.Document;
 
-            // Get the user's drawn scope box using the provided method
-            Element userDrawnScopeBox = GetSelectedScopeBox(doc, uiapp);
-
-            // Check if a valid scope box was selected
-            if (userDrawnScopeBox == null)
+            try
             {
-                return Result.Failed; // If not no valid scope box selected, Teminate the command
-            }
 
-            // Get the dimensions of the userScopeBoxBoundingBox
-            BoundingBoxXYZ userScopeBoxBoundingBox = userDrawnScopeBox.get_BoundingBox(null);
-            double newScopeBoxX = userScopeBoxBoundingBox.Max.X - userScopeBoxBoundingBox.Min.X;
-            double newScopeBoxY = userScopeBoxBoundingBox.Max.Y - userScopeBoxBoundingBox.Min.Y;
+                // Get the user's drawn scope box using the provided method
+                Element userDrawnScopeBox = GetSelectedScopeBox(doc, uiapp);
 
-            // Define the number of rows and columns for inner scope boxes
-            int rows = 3; // Number of rows
-            int columns = 3; // Number of columns
-
-            // Define the Overlap of the scope boxes
-            double HorizontalFeetOverlap = 5;
-            double VerticalFeetOverlap = 5;
-
-            // Define the Name for the new Scope Box
-            string scopeBoxBaseName = "My ScopeBox";
-            char nameChar = 'A';
-
-            using (Transaction transaction = new Transaction(doc))
-            {
-                // Start the transaction to create inner scope boxes and delete the original one
-                transaction.Start("Create Grid Scope Boxes and Delete Original");
-
-                // Calculate the starting point for rows and columns
-                XYZ startingPoint = new XYZ(0, 0, 0);
-
-                // Iterate through rows and columns to create the grid of scope boxes
-                for (int i = 0; i < rows; i++)
+                // Check if a valid scope box was selected
+                if (userDrawnScopeBox == null)
                 {
-                    for (int j = 0; j < columns; j++)
-                    {
-                        // Calculate the origin for the new scope box with overlaps
-                        // Note: Adding j * (newScopeBoxX - HorizontalFeetOverlap) to move Horizontally
-                        // Note: Subtracting i * (newScopeBoxY - VerticalFeetOverlap) to move vertically
-                        XYZ origin = new XYZ(
-                                                startingPoint.X + j * (newScopeBoxX - HorizontalFeetOverlap),
-                                                startingPoint.Y - i * (newScopeBoxY - VerticalFeetOverlap),
-                                                startingPoint.Z
-                                            );
-
-                        // Use ElementTransformUtils.CopyElements to replicate the outer scope box
-                        ICollection<ElementId> copiedScopeBoxes = ElementTransformUtils.CopyElements(
-                                                                        doc,
-                                                                        new List<ElementId> { userDrawnScopeBox.Id },
-                                                                        XYZ.Zero
-                                                                    );
-
-                        // Iterate through the copied scope boxes to move them to their respective positions
-                        foreach (ElementId copiedScopeBoxId in copiedScopeBoxes)
-                        {
-                            Element copiedScopeBox = doc.GetElement(copiedScopeBoxId);
-                            copiedScopeBox.Name = $"{scopeBoxBaseName} {nameChar}";  // Rename New Scope Box
-                            nameChar++; // Increase the Char value for nameChar
-                            ElementTransformUtils.MoveElement(doc, copiedScopeBoxId, origin); // Move the new Scope Box
-                        }
-                    }
+                    return Result.Failed; // If not no valid scope box selected, Teminate the command
                 }
 
-                // Delete the original user-drawn scope box
-                doc.Delete(userDrawnScopeBox.Id);
+                // Get the dimensions of the userScopeBoxBoundingBox
+                BoundingBoxXYZ userScopeBoxBoundingBox = userDrawnScopeBox.get_BoundingBox(null);
+                double newScopeBoxX = userScopeBoxBoundingBox.Max.X - userScopeBoxBoundingBox.Min.X;
+                double newScopeBoxY = userScopeBoxBoundingBox.Max.Y - userScopeBoxBoundingBox.Min.Y;
 
-                // Commit the transaction
-                transaction.Commit();
+
+
+
+                // ================== Form data ===================
+                // Access the controls from the form
+                ScopeBoxGridForm form = new ScopeBoxGridForm();
+                form.ShowDialog();
+
+                // Read values from textboxes
+                int rows = int.Parse(form.txtRows.Text);
+                int columns = int.Parse(form.txtColumns.Text);
+                double HorizontalFeetOverlap = double.Parse(form.txtHorizontalOverlap.Text);
+                double VerticalFeetOverlap = double.Parse(form.txtVerticalOverlap.Text);
+                string scopeBoxBaseName = form.txtBaseScopeBoxName.Text;
+                //Char nameChar = char.Parse(form.txtNameChar.Text);
+                // ================== Form data End===================
+
+                //// Define the number of rows and columns for inner scope boxes
+                //int rows = 3; // Number of rows
+                //int columns = 3; // Number of columns
+
+                //// Define the Overlap of the scope boxes
+                //double HorizontalFeetOverlap = 5;
+                //double VerticalFeetOverlap = 5;
+
+                //// Define the Name for the new Scope Box
+                //string scopeBoxBaseName = "My ScopeBox";
+                //char nameChar = 'A';
+
+
+
+                var listOfScopeBoxesCreated = new List<Element>();
+
+                using (Transaction transaction = new Transaction(doc))
+                {
+                    // Start the transaction to create inner scope boxes and delete the original one
+                    transaction.Start("Create Grid Scope Boxes and Delete Original");
+                    userDrawnScopeBox.Name = scopeBoxBaseName + " 0";
+
+                    // Calculate the starting point for rows and columns
+                    XYZ startingPoint = new XYZ(0, 0, 0);
+
+                    // Iterate through rows and columns to create the grid of scope boxes
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            // Calculate the origin for the new scope box with overlaps
+                            // Note: Adding j * (newScopeBoxX - HorizontalFeetOverlap) to move Horizontally
+                            // Note: Subtracting i * (newScopeBoxY - VerticalFeetOverlap) to move vertically
+                            XYZ origin = new XYZ(
+                                                    startingPoint.X + j * (newScopeBoxX - HorizontalFeetOverlap),
+                                                    startingPoint.Y - i * (newScopeBoxY - VerticalFeetOverlap),
+                                                    startingPoint.Z
+                                                );
+
+                            // Use ElementTransformUtils.CopyElements to replicate the outer scope box
+                            ICollection<ElementId> copiedScopeBoxes = ElementTransformUtils.CopyElements(
+                                                                            doc,
+                                                                            new List<ElementId> { userDrawnScopeBox.Id },
+                                                                            XYZ.Zero
+                                                                        );
+
+                            // Iterate through the copied scope boxes to move them to their respective positions
+                            foreach (ElementId copiedScopeBoxId in copiedScopeBoxes)
+                            {
+                                Element copiedScopeBox = doc.GetElement(copiedScopeBoxId);
+                                //copiedScopeBox.Name = $"{scopeBoxBaseName} {nameChar}";  // Rename New Scope Box
+                                //nameChar++; // Increase the Char value for nameChar
+                                ElementTransformUtils.MoveElement(doc, copiedScopeBoxId, origin); // Move the new Scope Box
+                                listOfScopeBoxesCreated.Add(copiedScopeBox);
+                            }
+                        }
+                    }
+
+                    // Delete the original user-drawn scope box
+                    doc.Delete(userDrawnScopeBox.Id);
+
+                    // Commit the transaction
+                    transaction.Commit();
+                }
+                TaskDialog.Show("Info", $"{listOfScopeBoxesCreated.Count} Scopeboxs Created");
+
             }
+            catch (Exception e)
+            {
 
+                TaskDialog.Show("Error", $"{e.Message}");
+
+            }
             // Return the result indicating success
             return Result.Succeeded;
         }
---
+++ ScopeBoxes/Forms/ScopeBoxGridForm.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ScopeBoxes.Forms
+{
+    /// <summary>
+    /// Interaction logic for ScopeBoxGridForm.xaml
+    /// </summary>
+    public partial class ScopeBoxGridForm : Window
+    {
+        public ScopeBoxGridForm()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Center the window on the screen
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void BtnCreateScopeBoxGrid_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
